6797ed922e7981bede3586073a80c8952f026c6f
dairo nieto paez <daironieto6000@hotmail.com>
2024-10-21 11:04:17 -0500

    go router oficial implementado

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,12 +1,42 @@
+
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:cinemapedia/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
+  ShellRoute(
+    builder: (context, state, child) {
+      return HomeScreen(childView: child);
+    },
+    routes: [
+      GoRoute(path: '/',builder: (context, state) {
+        return const HomeView();
+      },routes: [
+         GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieID = state.params['id'] ?? 'no-id';
+            return MovieScreen(movieId: movieID);
+          },
+        ),
+      ]),
+        GoRoute(path: '/favorites',builder: (context, state) {
+        return const FavoritesView();
+      })
+    ]
+  )
+
+
+
+
+/*   
+RUTAS PADRE/HIJO
+GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) => const HomeScreen(childView: HomeView()),
       routes: [
         GoRoute(
           path: 'movie/:id',
@@ -16,5 +46,5 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
             return MovieScreen(movieId: movieID);
           },
         ),
-      ]),
+      ]), */
 ]);
